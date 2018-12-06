3af39f3059cc0d5a43ecfe52175e43c4372a64dc
monzo94 <dmonzonis@protonmail.com>
2018-12-06 21:50:46 +0100

    Dimension as cli input instead of constant

--- generate-random-grid.py
+++ generate-random-grid.py
@@ -1,25 +1,30 @@
+import argparse
 import csv
 from random import choice
 
-DIMENSIONS = 1000
 
-
-def generate_grid():
+def generate_grid(dimension):
     pool = list(range(1, 11, 2)) + [-1]
     with open('randomgrid.csv', 'w') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         # Write dimensions
-        writer.writerow([DIMENSIONS, DIMENSIONS])
+        writer.writerow([dimension, dimension])
         # Don't need start and goal positions
         writer.writerow([0] * 4)
         # Generate grid with random weights from the pool
-        for y in range(DIMENSIONS):
-            print(f"Writing row {y+1} of {DIMENSIONS}", sep=' ', end='\r', flush=True)
-            writer.writerow([choice(pool) for _ in range(DIMENSIONS)])
+        for y in range(dimension):
+            print(f"Writing row {y+1} of {dimension}", sep=' ', end='\r', flush=True)
+            writer.writerow([choice(pool) for _ in range(dimension)])
 
 
 def main():
-    generate_grid()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dimension', type=int,
+                        help="dimensions of the grid", required=True)
+    args = parser.parse_args()
+
+    dimension = args.dimension
+    generate_grid(dimension)
 
 
 if __name__ == "__main__":
