79a491068dcb9d6d95086de53d2b67efe74d21b9
monzo94 <dmonzonis@protonmail.com>
2018-12-08 21:16:46 +0100

    Fix cartesian coords

--- src/geolocationgraph.cpp
+++ src/geolocationgraph.cpp
@@ -4,10 +4,10 @@
 void Geolocation::computeCartesianCoordinates()
 {
     // Theta and Phi angles must be in radians
-    double theta = (90 - (latitude / 1e6)) * M_PI / 180;
+    double theta = (latitude / 1e6) * M_PI / 180;
     double phi = (longitude / 1e6) * M_PI / 180;
     // Compute Cartesian coordinates
-    x = radius * std::sin(theta) * std::cos(phi);
-    y = radius * std::sin(theta) * std::sin(phi);
-    z = radius * std::cos(theta);
+    x = radius * std::cos(theta) * std::cos(phi);
+    y = radius * std::cos(theta) * std::sin(phi);
+    z = radius * std::sin(theta);
 }
